efe0779f538ecfc25b86a533a623d1536fa69180
mohamed-aziz <medazizknani@gmail.com>
2016-03-05 17:00:57 +0000

    Test avancement

--- avancement.php
+++ avancement.php
@@ -3,6 +3,9 @@
 </style>
 <?php
 
+function nb($s) {
+return (intval(mysql_fetch_assoc(mysql_query("SELECT COUNT(*) as total FROM eleve where CodeMatiere=\"$s\""))['total']) / $nbeleves)* 100;
+}
 
 mysql_connect('localhost', 'root', 'mohamed');
 mysql_select_db('BDdevoir1');
@@ -10,7 +13,12 @@ mysql_select_db('BDdevoir1');
 echo "<table>";
 // Obtenir le numero d'eleves
 $q = mysql_query("SELECT COUNT(*) as total FROM Eleve");
-echo mysql_fetch_assoc($q)['total'];
+$nbeleves = mysql_fetch_assoc($q)['total'];
+$nbAlgo =nb('Algo');
+$nbTic = nb('TIC');
+$nbBD = nb('BD');
+echo "<tr><td>Algorithmique</td><td>BD</td><td>TIC</td></tr>";
+echo "<tr><td>$nbAlgo</td> <td>$nbBD</td> <td>$nbTic</td> </tr>"
 echo "</table>";
 mysql_close();
 ?>
