acba196f1330541675eec0121664806c693df37c
mohamed-aziz <medazizknani@gmail.com>
2016-03-05 11:59:50 +0000

    Test de Saisie

--- saisir.php
+++ saisir.php
@@ -1,4 +1,10 @@
 <?php
-  mysql_connect('localhost', 'root', '');
-  
+mysql_connect('localhost', 'root', 'mohamed');
+mysql_select_db('BDdevoir1');
+$s = "2222";
+$q = mysql_query('SELECT * From Eleve WHERE Numero=$s');
+if (!($q)) {
+    echo "Eleve Non Inscrit<br>";
+}
+mysql_close();
 ?>
