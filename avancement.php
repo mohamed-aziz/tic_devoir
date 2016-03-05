57edc7581e5e2e5e7f289ae32a49464a420018b8
mohamed-aziz <medazizknani@gmail.com>
2016-03-05 17:15:58 +0000

    Test avancement

--- avancement.php
+++ avancement.php
@@ -4,8 +4,7 @@
 <?php
 
 function nb($s, $nbeleves) {
-var_dump(mysql_fetch_assoc(mysql_query("SELECT COUNT(*) as total FROM Eleve WHERE CodeMatiere='Algo'")));
-return (intval(mysql_fetch_assoc(mysql_query("SELECT COUNT(*) as total FROM Eleve where CodeMatiere=\"$s\""))['total']) / $nbeleves) * 100;
+return (intval(mysql_fetch_assoc(mysql_query("SELECT COUNT(*) as total FROM Note where CodeMatiere=\"$s\""))['total']) / $nbeleves) * 100;
 }
 
 mysql_connect('localhost', 'root', 'mohamed');
