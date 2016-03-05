4be12df5135c84c156db0510bf36d54ddda2245a
mohamed-aziz <medazizknani@gmail.com>
2016-03-05 16:49:28 +0000

    Test avancement

--- avancement.php
+++ avancement.php
@@ -10,7 +10,7 @@ mysql_select_db('BDdevoir1');
 echo "<table>";
 // Obtenir le numero d'eleves
 $q = mysql_query("SELECT COUNT(*) as total FROM Eleve");
-ecoh mysql_fetch_assoc($q)['total'];
+echo mysql_fetch_assoc($q)['total'];
 echo "</table>";
 mysql_close();
 ?>
