95b92a1a9d2543d1dc9fc5a740037be6ac835686
mohamed-aziz <medazizknani@gmail.com>
2016-03-05 12:00:42 +0000

    Test de Saisie

--- saisir.php
+++ saisir.php
@@ -1,7 +1,7 @@
 <?php
 mysql_connect('localhost', 'root', 'mohamed');
 mysql_select_db('BDdevoir1');
-$s = "2222";
+$s = "00017";
 $q = mysql_query('SELECT * From Eleve WHERE Numero=$s');
 if (!($q)) {
     echo "Eleve Non Inscrit<br>";
